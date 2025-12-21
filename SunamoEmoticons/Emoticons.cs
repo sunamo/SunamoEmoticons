38fd8c86c3b45ddb572d64dab835758a1792ed38
Radek Jancik <radek.jancik@usys.cz>
2025-12-21 20:50:06 +0100

    25.12.21.1 feat: Added net10.0 to TargetFrameworks

--- SunamoEmoticons/Emoticons.cs
+++ SunamoEmoticons/Emoticons.cs
@@ -1,7 +1,4 @@
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
 namespace SunamoEmoticons;
-
 
 /// <summary>
 /// all must be internal due to object.GetFields()
